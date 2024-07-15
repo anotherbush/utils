1f4c85ddaa10dc0e6e1a1daf8f8a0fdb00bbe708
timchen <timchen10001@gmail.com>
2024-07-15 21:44:44 +0800

    refactor(next): refactor `use-local-storage`

--- packages/next/src/use-local-storage.ts
+++ packages/next/src/use-local-storage.ts
@@ -1,40 +1,56 @@
 import { UseLocalStorage } from '@anotherbush/react';
 import {
   getLocalStorageItem,
+  hasLocalStorageItem,
   isBrowser,
   removeLocalStorageItem,
   setLocalStorageItem,
   watchLocalStorageItem,
 } from '@anotherbush/utils';
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { filter, tap } from 'rxjs';
+import { useHydrated } from './use-hydrated';
+
+export interface UseNextLocalStorage<T> extends UseLocalStorage<T> {
+  hydrated: boolean;
+}
 
 export function useLocalStorage<T>(
   key: string,
   fallback: T
-): UseLocalStorage<T>;
-export function useLocalStorage<T>(key: string): UseLocalStorage<T | null>;
+): UseNextLocalStorage<T>;
+export function useLocalStorage<T>(key: string): UseNextLocalStorage<T | null>;
 export function useLocalStorage<T>(
   key: string,
   fallback?: T
-): UseLocalStorage<T | null>;
+): UseNextLocalStorage<T | null>;
 export function useLocalStorage<T>(
   key: string,
   fallback?: T
-): UseLocalStorage<T | null> {
+): UseNextLocalStorage<T | null> {
+  const fallbackRef = useRef(fallback);
+  const hydrated = useHydrated();
   const [data, setData] = useState<T | null>(fallback ?? null);
 
   useEffect(() => {
-    setData(getLocalStorageItem<T>(key, fallback));
-    const sub = watchLocalStorageItem<T>(key, fallback)
+    fallbackRef.current = fallback;
+  }, [fallback]);
+
+  useEffect(() => {
+    if (!hydrated) return;
+
+    if (hasLocalStorageItem(key)) {
+      setData(getLocalStorageItem<T>(key, fallbackRef.current));
+    }
+    const sub = watchLocalStorageItem<T>(key, fallbackRef.current)
       .pipe(filter(isBrowser), tap(setData))
       .subscribe();
     return () => sub.unsubscribe();
-  }, [key]);
+  }, [key, hydrated]);
 
   const set = <TT extends T>(next: TT) => setLocalStorageItem(key, next);
 
   const remove = () => removeLocalStorageItem(key);
 
-  return { data, set, remove };
+  return { data, hydrated, set, remove };
 }
