ecdc871bb2e870b7821a5f5ff24438fb3247b244
timchen <timchen10001@gmail.com>
2024-07-21 23:59:47 +0800

    fix(react): add `use-watch-store` hook

---
+++ packages/react/src/hooks/use-watch-store.ts
@@ -0,0 +1,28 @@
+import { ObservableStore } from '@anotherbush/utils';
+import { useEffect, useState } from 'react';
+import { tap } from 'rxjs';
+
+export function useWatchStore<
+  Store extends ObservableStore<any>,
+  T extends Store['value']
+>(store: Store): T;
+export function useWatchStore<
+  Store extends ObservableStore<any>,
+  T extends Store['value'],
+  Key extends keyof T
+>(store: Store, key: Key): T[Key];
+export function useWatchStore<
+  Store extends ObservableStore<any>,
+  T extends Store['value'],
+  Key extends keyof T
+>(store: Store, key?: Key): T | T[Key] {
+  const [data, setData] = useState(() => store.get(key));
+
+  useEffect(() => {
+    setData(store.get(key));
+    const sub = store.watch(key).pipe(tap(setData)).subscribe();
+    return () => sub.unsubscribe();
+  }, [key]);
+
+  return data;
+}
