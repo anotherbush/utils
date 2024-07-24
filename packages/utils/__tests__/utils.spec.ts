4c28ef7b798e63ea3564e12fb7bb2deb23dcd0db
timchen <timchen10001@gmail.com>
2024-07-24 15:28:25 +0800

    feat(utils/cache): add cache testing

--- packages/utils/__tests__/utils.spec.ts
+++ packages/utils/__tests__/utils.spec.ts
@@ -1,8 +1,8 @@
-import { skip, take } from 'rxjs';
-import { stringifyId } from '@anotherbush/utils';
+import { LFUCache, LRUCache, ObservableCache } from '@anotherbush/utils';
+import { skip, take, tap } from 'rxjs';
 import { ObservableStore } from '../src/observable-store';
 
-it('ObservableStore', () => {
+describe('ObservableStore', () => {
   interface MyClientStoreValue {
     loading: boolean;
     name: string;
@@ -13,13 +13,10 @@ it('ObservableStore', () => {
     name: 'Tim',
   });
 
-  expect(
-    stringifyId(store.value) ===
-      stringifyId({
-        loading: false,
-        name: 'Tim',
-      })
-  ).toBe(true);
+  expect(store.value).toEqual({
+    loading: false,
+    name: 'Tim',
+  });
 
   let watchAllUntilChangeOneTimeReceived = 0;
   store
@@ -28,55 +25,46 @@ it('ObservableStore', () => {
     .subscribe((client) => {
       ++watchAllUntilChangeOneTimeReceived;
       if (watchAllUntilChangeOneTimeReceived === 1) {
-        expect(
-          stringifyId(client) ===
-            stringifyId({
-              loading: false,
-              name: 'Tim',
-            })
-        ).toBe(true);
+        expect(client).toEqual({
+          loading: false,
+          name: 'Tim',
+        });
       } else if (watchAllUntilChangeOneTimeReceived === 2) {
-        expect(
-          stringifyId(client) ===
-            stringifyId({
-              loading: false,
-              name: 'Amy',
-            })
-        ).toBe(true);
+        expect(client).toEqual({
+          loading: false,
+          name: 'Amy',
+        });
       } else {
-        expect(false).toBe(true);
+        expect(watchAllUntilChangeOneTimeReceived).toEqual(2);
       }
     });
 
   store.dispatch('name', (prevName) => {
-    expect(prevName === 'Tim').toBe(true);
+    expect(prevName).toEqual('Tim');
     return 'Amy';
   });
 
-  const watchNameOnChange = store
+  store
     .watch('name')
     .pipe(skip(1), take(1))
     .subscribe((nextName) => {
-      expect(nextName === 'Bob').toBe(true);
+      expect(nextName).toEqual('Bob');
     });
 
   store.dispatch('name', (prevName) => {
-    expect(prevName === 'Amy').toBe(true);
+    expect(prevName).toEqual('Amy');
     return 'Bob';
   });
 
-  const watchOptionalFieldOnChange = store
+  store
     .watch('optionalField')
     .pipe(skip(1), take(1))
     .subscribe((nextOptionalField) => {
-      expect(
-        stringifyId(nextOptionalField) ===
-          stringifyId({ dynamicallyPatched: ':)' })
-      ).toBe(true);
+      expect(nextOptionalField).toEqual({ dynamicallyPatched: ':)' });
     });
 
   store.dispatch('optionalField', (prevOptionalField) => {
-    expect(prevOptionalField === undefined).toBe(true);
+    expect(prevOptionalField).toBeUndefined();
     return { dynamicallyPatched: ':)' };
   });
 
@@ -84,7 +72,7 @@ it('ObservableStore', () => {
     .watch('name')
     .pipe(skip(1), take(1))
     .subscribe((nextName) => {
-      expect(nextName === 'John').toBe(true);
+      expect(nextName).toEqual('John');
     });
 
   store.dispatch((prev) => ({
@@ -92,21 +80,136 @@ it('ObservableStore', () => {
     name: 'John',
   }));
 
-  expect(
-    stringifyId(store.get()) ===
-      stringifyId({
-        loading: false,
-        name: 'John',
-        optionalField: { dynamicallyPatched: ':)' },
-      })
-  ).toBe(true);
+  expect(store.get()).toEqual({
+    loading: false,
+    name: 'John',
+    optionalField: { dynamicallyPatched: ':)' },
+  });
 
   store
     .watch('name')
     .pipe(skip(1), take(1))
     .subscribe((nextName) => {
-      expect(nextName === 'TimChen').toBe(true);
+      expect(nextName).toEqual('TimChen');
     });
 
   store.dispatch('name', 'TimChen');
+});
+
+describe('ObservableCache', () => {
+  /** ObservableCache with constructor */
+  it('ObservableCache with constructor', () => {
+    const objectRef1: never[] = [];
+    const cache$ = new ObservableCache<Array<any>, number>(LRUCache, 2);
+
+    expect(cache$).toBeInstanceOf(ObservableCache);
+
+    cache$.set(objectRef1, 1);
+
+    expect(cache$.has(objectRef1)).toBeTruthy();
+    expect(cache$.get(objectRef1)).toEqual(1);
+    expect(cache$.size).toEqual(1);
+    expect(cache$.capacity).toEqual(2);
+
+    expect(cache$).toHaveProperty<ObservableCache>(
+      'capacity' as keyof ObservableCache
+    );
+    expect(cache$).toHaveProperty<ObservableCache>(
+      'size' as keyof ObservableCache
+    );
+    expect(cache$).toHaveProperty<ObservableCache>(
+      'delete' as keyof ObservableCache
+    );
+    expect(cache$).toHaveProperty<ObservableCache>(
+      'get' as keyof ObservableCache
+    );
+    expect(cache$).toHaveProperty<ObservableCache>(
+      'has' as keyof ObservableCache
+    );
+    expect(cache$).toHaveProperty<ObservableCache>(
+      'set' as keyof ObservableCache
+    );
+    expect(cache$).toHaveProperty<ObservableCache>(
+      'setCapacity' as keyof ObservableCache
+    );
+    expect(cache$).toHaveProperty<ObservableCache>(
+      'watch' as keyof ObservableCache
+    );
+
+    expect(cache$.size).toEqual(1);
+  });
+
+  /** LRUCache */
+  it('LRUCache', () => {
+    const lruCache = new LRUCache<'1' | '2', number>(10);
+    const lruCache$ = new ObservableCache(lruCache);
+
+    expect(lruCache$.has('1')).toBeFalsy();
+
+    lruCache$
+      .watch('1')
+      .pipe(
+        take(1),
+        tap((val) => {
+          expect(val).toEqual(1);
+        })
+      )
+      .subscribe();
+
+    lruCache$.set('1', 1);
+
+    expect(lruCache$.has('1')).toBeTruthy();
+    expect(lruCache$.get('1')).toEqual(1);
+  });
+
+  /** LFUCache */
+  it('LFUCache', () => {
+    const lfuCache = new LFUCache<'1' | '2', number>(1);
+    const lfuCache$ = new ObservableCache(lfuCache);
+
+    expect(lfuCache$).toBeInstanceOf(ObservableCache);
+
+    expect(lfuCache$.has('1')).toBeFalsy();
+
+    lfuCache$
+      .watch('1')
+      .pipe(
+        take(1),
+        tap((val) => {
+          expect(val).toEqual(1);
+        })
+      )
+      .subscribe();
+
+    lfuCache$.set('1', 1);
+
+    expect(lfuCache$.has('1')).toBeTruthy();
+    expect(lfuCache$.get('1')).toEqual(1);
+
+    const watchKey1Cb = (val: number | undefined) => {
+      // console.log('1', val);
+      /** be truncated out by Key2 set to 2 */
+      expect(val).toBeUndefined();
+    };
+    const spiedWatchKey1Cb = jest.fn(watchKey1Cb);
+    lfuCache$.watch('1').pipe(take(1)).subscribe(watchKey1Cb);
+    jest.fn(() => {
+      expect(spiedWatchKey1Cb).toHaveBeenCalled();
+      expect(spiedWatchKey1Cb).toHaveBeenCalledTimes(1);
+    });
+
+    const watchKey2Cb = (val: number | undefined) => {
+      // console.log('watchKey2Cb', val);
+      expect(val).toEqual(2);
+    };
+    const spiedWatchKey2Cb = jest.fn(watchKey2Cb);
+    lfuCache$.watch('2').pipe(take(1), tap(watchKey2Cb)).subscribe();
+    lfuCache$.set('2', 2);
+    jest.fn(() => {
+      expect(spiedWatchKey2Cb).toHaveBeenCalled();
+      expect(spiedWatchKey2Cb).toHaveBeenCalledTimes(1);
+    });
+
+    expect(lfuCache$.size).toEqual(1);
+  });
 });
