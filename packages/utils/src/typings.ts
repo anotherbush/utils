c00a7d402431cc062742edc1ff7cafbeb6ad77bd
timchen <timchen10001@gmail.com>
2024-07-24 17:32:10 +0800

    feat(utils): update `utils/typings`

--- packages/utils/src/typings.ts
+++ packages/utils/src/typings.ts
@@ -1,3 +1,5 @@
+import type { Observable } from 'rxjs';
+
 export type ObjectType = { [key: string | symbol]: any };
 
 export type ArrayType = { [key: number]: any };
@@ -5,3 +7,12 @@ export type ArrayType = { [key: number]: any };
 export type ValidKey = string | number | symbol | object;
 
 export type ConstructorOf<T> = { new (...args: any[]): T };
+
+/**
+ * Extension of an object that can observe its mutation events.
+ */
+export interface WatchableObject<T extends ObjectType> {
+  /** Return a hot observable to receive mutation events. */
+  watch<Key extends keyof T>(key: Key): Observable<T[Key]>;
+  watch<Key extends keyof T>(key?: Key): Observable<T[Key] | undefined>;
+}
